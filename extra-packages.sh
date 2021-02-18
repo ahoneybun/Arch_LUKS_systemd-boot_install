99595e2e3f125c7e5d35000475273a006a15d16a
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2021-02-17 21:29:02 -0700

    add Pika Backups and it's deps

--- extra-packages.sh
+++ extra-packages.sh
@@ -10,6 +10,26 @@ echo "-------------------------------------"
 sleep 2
 clear
 
+echo ""
 echo "- Installing Noto Font bundles (helpful for Spot[ify] songs with foreign letters)"
+echo ""
 
 sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts
+
+echo ""
+echo "- Installing build deps for Pika Backup"
+echo ""
+
+sudo pacman -S meson python-llfuse borg
+
+echo ""
+echo "- Downloading Pika Backup from AUR"
+echo ""
+
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/pika-backup.tar.gz
+tar -xf pika-backup.tar.gz
+
+cd pika-backup
+makepkg -ic
+cd ..
+
