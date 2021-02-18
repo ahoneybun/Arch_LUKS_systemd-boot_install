7fd2873a090cab36a2d3097680363d9f5c105a84
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2021-02-17 21:23:02 -0700

    adding a script for some packages I need/want

---
+++ extra-packages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Intro
+# ---
+
+clear
+echo "-------------------------------------"
+echo "| Welcome to version 0.2 of the EPI |"
+echo "-------------------------------------"
+sleep 2
+clear
+
+echo "- Installing Noto Font bundles"
+
+sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts
