f6fdf8c5419a4dde94fc036e7e9874c15b29a0f6
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2021-02-19 18:55:08 -0700

    adding more packages/3

--- extra-packages.sh
+++ extra-packages.sh
@@ -17,10 +17,16 @@ echo ""
 sudo pacman -S --needed noto-fonts-cjk noto-fonts-emoji noto-fonts
 
 echo ""
-echo "- Installing Firefox, Thunderbird, Tilix, Fish, Flatpak, neofetch, Telegram"
+echo "- Installing Firefox, Thunderbird, Tilix, Fish, Flatpak, Telegram"
 echo ""
 
-sudo pacman -S --needed firefox thunderbird tilix, fish, flatpak, neofetch, telegram-desktop
+sudo pacman -S --needed firefox thunderbird tilix, fish, flatpak, telegram-desktop
+
+echo ""
+echo "- Installing development packages"
+echo ""
+
+sudo pacman -S --needed neofetch rust tree rygel typescript
 
 echo ""
 echo "- Downloading Pika Backup from AUR"
@@ -28,7 +34,24 @@ echo ""
 
 git clone https://aur.archlinux.org/pika-backup.git
 
+echo ""
+echo "- Building Pika Backup"
+echo ""
+
 cd pika-backup
 makepkg -srcif
 cd ..
 
+echo ""
+echo "- Downloading Visual Studio Code from AUR"
+echo ""
+
+git clone https://aur.archlinux.org/visual-studio-code-bin
+
+echo ""
+echo "- Building Visual Studio Code"
+echo ""
+
+cd visual-studio-code-bin
+makepkg -srcif
+cd ..
