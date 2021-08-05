a44427ee954d470039798f65acb549c8e55b3fab
Aaron Honeycutt <aaron@system76.com>
2021-08-05 10:39:48 -0600

    update toPopify Script

--- pop-packages.sh
+++ pop-packages.sh
@@ -15,237 +15,118 @@ echo "| Installing dependencies |"
 echo "---------------------------"
 sleep 2
 
-sudo pacman -S --needed base-devel git linux-headers
+sudo pacman -S --needed base-devel git meson sassc gnome-icon-theme 
 
-echo "---------------------"
-echo "| System76 Firmware |"
-echo "---------------------"
+echo "-----------------"
+echo "| Pop GTK Theme |"
+echo "-----------------"
 sleep 2
 
 echo ""
 echo "- Downloading from the AUR"
 echo ""
 
-git clone https://aur.archlinux.org/system76-firmware.git
+git clone https://aur.archlinux.org/pop-gtk-theme.git
 
 echo ""
 echo "- Entering the directory and building"
 echo ""
 
-cd system76-firmware
-makepkg -srcif
+cd pop-gtk-theme
+makepkg -si
 cd ..
-
-echo ""
-echo "- Enabling and starting services"
-echo ""
-
-sudo systemctl enable --now system76-firmware-daemon
 
 sleep 5
 
-echo "-----------------------------"
-echo "| System76 Firmware Manager |"
-echo "-----------------------------"
+echo "--------------------"
+echo "| Roboto Slab Font |"
+echo "--------------------"
 sleep 2
 
 echo ""
 echo "- Downloading from the AUR"
 echo ""
 
-git clone https://aur.archlinux.org/firmware-manager.git
+git clone https://aur.archlinux.org/ttf-roboto-slab.git
 
 echo ""
 echo "- Entering the directory and building"
 echo ""
 
-cd firmware-manager
-makepkg -srcif
+cd ttf-roboto-slab
+makepkg -si
 cd ..
 
 sleep 5
 
-echo "---------------------------------"
-echo "| Does this have Open Firmware? |"
-echo "| - Yes                         |"
-echo "| - No                          |"
-echo "---------------------------------"
-
-echo -n ": "; read closed
-case "$closed" in
-
-Yes) 
-   echo ""
-   echo "- Downloading from the AUR"
-   echo ""
-
-   git clone https://aur.archlinux.org/system76-dkms.git
-
-   echo ""
-   echo "- Entering the directory and building"
-   echo ""
-
-   cd system76-dkms
-   makepkg -srcif
-   cd ..
-   ;;
-No) break
-
-esac
-
-echo "---------------------------------"
-echo "| Does this have Open Firmware? |"
-echo "| - Yes                         |"
-echo "| - No                          |"
-echo "---------------------------------"
-
-echo -n ": "; read closed
-case "$closed" in
-
-Yes) 
-   echo ""
-   echo "- Downloading from the AUR"
-   echo ""
-
-   git clone https://aur.archlinux.org/system76-acpi-dkms.git
-
-   echo ""
-   echo "- Entering the directory and building"
-   echo ""
-
-   cd system76-acpi-dkms
-   makepkg -srcif
-   cd ..
-   ;;
-No) break
-
-esac
-
-echo "--------------------------"
-echo "| System76 Power package |"
-echo "--------------------------"
+echo "-------------"
+echo "| Pop Fonts |"
+echo "-------------"
 sleep 2
 
 echo ""
 echo "- Downloading from the AUR"
 echo ""
 
-git clone https://aur.archlinux.org/system76-power.git
+git clone https://aur.archlinux.org/pop-fonts.git
 
 echo ""
 echo "- Entering the directory and building"
 echo ""
 
-cd system76-power
-makepkg -srcif
+cd pop-fonts
+makepkg -si   
 cd ..
-
-echo ""
-echo "- Enabling and starting services"
-echo ""
-
-sudo systemctl enable --now system76-power
 
 sleep 5
 
-echo "----------------------------------------"
-echo "| System76 Power GNOME Shell Extension |"
-echo "----------------------------------------"
+echo "------------------"
+echo "| Pop Icon Theme |"
+echo "------------------"       
 sleep 2
 
 echo ""
 echo "- Downloading from the AUR"
 echo ""
 
-git clone https://aur.archlinux.org/gnome-shell-extension-system76-power-git.git
+git clone https://aur.archlinux.org/pop-icon-theme.git      
 
 echo ""
 echo "- Entering the directory and building"
 echo ""
 
-cd gnome-shell-extension-system76-power-git
-makepkg -srcif
+cd pop-icon-theme
+makepkg -si
 cd ..
 
 sleep 5
 
-echo "---------------------------"
-echo "| System76 Driver package |"
-echo "---------------------------"
+echo "-------------"
+echo "| Pop Theme |"
+echo "-------------"       
 sleep 2
 
 echo ""
 echo "- Downloading from the AUR"
 echo ""
 
-git clone https://aur.archlinux.org/system76-driver.git
+git clone https://aur.archlinux.org/pop-theme.git      
 
 echo ""
 echo "- Entering the directory and building"
 echo ""
 
-cd system76-driver
-makepkg -srcif
+cd pop-theme
+makepkg -si
 cd ..
 
-echo ""
-echo "- Enabling and starting services"
-echo ""
-
-sudo systemctl enable --now system76
-
 sleep 5
-
-echo "----------------------------"
-echo "| Is this a Thelio system? |"
-echo "| - Yes                    |"
-echo "| - No                     |"
-echo "----------------------------"
-
-echo -n ": "; read thelio
-case "$thelio" in
-
-Yes) git clone https://aur.archlinux.org/system76-io-dkms.git
-   cd system76-io-dkms
-   makepkg -srcif
-   cd ..
-   ;;
-No) break
-
-esac
-
-echo "----------------------------------------"
-echo "| Does your system have an OLED panel? |"
-echo "| - Yes                                |"
-echo "| - No                                 |"
-echo "----------------------------------------"
-
-echo -n ": "; read oled
-case "$oled" in
-
-Yes) git clone https://aur.archlinux.org/system76-oled.git
-   cd system76-oled
-   makepkg -srcif
-   cd ..
-   ;;
-No) exit 1
-
-esac
-
-# Add user to adm group
-sudo usermod -aG adm $USER
-
-echo "You will need to reboot for the last command to take effect"
 
 # Clean up
 # ---
 
- sudo rm -r system76-firmware*
- sudo rm -r firmware-manager*
- sudo rm -r system76-dkms*
- sudo rm -r system76-power*
- sudo rm -r gnome-shell-extension-system76-power-git*
- sudo rm -r system76-driver*
- sudo rm -r system76-acpi-dkms*
- sudo rm -r system76-io-dkms*
- sudo rm -r system76-oled*
+ sudo rm -r pop-theme*
+ sudo rm -r pop-gtk-theme*
+ sudo rm -r pop-icon-theme*
+ sudo rm -r pop-fonts*
+ sudo rm -r ttf-roboto-slab*
